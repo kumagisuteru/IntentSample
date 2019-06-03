42e6e6b2583976e8b612fc2304d781c26bea4dc6
kumagisuteru <p2od58hx@s.okayama-u.ac.jp>
2019-06-03 17:31:58 +0900

    pre

---
+++ app/src/main/java/com/websarva/wings/android/intentsample/MenuThanksActivity.java
@@ -0,0 +1,33 @@
+package com.websarva.wings.android.intentsample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.TextView;
+
+import java.util.Map;
+
+public class MenuThanksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_thanks);
+
+
+        Intent intent = getIntent();
+        String menuName = intent.getStringExtra("menuName");
+        String menuPrice = intent.getStringExtra("menuPrice");
+        TextView tvMenuName = findViewById(R.id.tvMenuName);
+        TextView tvMenuPrice = findViewById(R.id.tvMenuPrice);
+
+        tvMenuName.setText(menuName);
+        tvMenuPrice.setText(menuPrice);
+    }
+    public void onBackButtonClick(View view){
+        finish();
+    }
+
+}
